8986b165ce9fb08cfe5b1705cae5c3482861b82d
abehlok2 <132627628+abehlok2@users.noreply.github.com>
2025-08-18 10:29:33 -0400

    chore: restrict lint to ts files

--- .eslint.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  env: { browser: true, node: true, es2021: true },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-};
